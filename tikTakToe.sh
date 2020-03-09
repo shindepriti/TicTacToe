af48e3373b04071844cb60c748e04595a8f33e42
shindepriti <pritishinde34@gmail.com>
2020-03-09 19:10:20 +0530

    ADDED:Function To reset Board And Display Board

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 echo "***Wel-Come To Tik-Tak-Toe game***"
 
+declare -a board
+board=(1 2 3 4 5 6 7 8 9)
+
+function displayBoard(){
+	echo "-----------"
+for ((i=0;i<9;i=i+3))
+do
+	echo " ${board[$i]} | ${board[$i+1]} | ${board[$i+2]}"
+	echo "-----------"
+done
+}
+displayBoard
