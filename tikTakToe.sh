d756b911263c419e92baa4f14de5ae8b73b87a23
shindepriti <pritishinde34@gmail.com>
2020-03-11 17:46:37 +0530

    ADDED:Check If Opponent player Win Then Block The Oppponent

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -x
-echo "Wel-Come To Tic-Tac-Toe game"
 
+#!/bin/bash -x
+echo "****WEL-COME TO TIC TAC TOE GAME****"
 declare -a board
 board=(1 2 3 4 5 6 7 8 9)
 
 #variable
 count=0
 
-#CONSTANT
+#constatnt
 MAXCELL=9
 
 function displayBoard(){
@@ -20,16 +20,16 @@ function displayBoard(){
 }
 
 function assignSymbol(){
-	if [[ $((RANDOM%2)) -eq 1 ]]
-	then	
+if [[ $((RANDOM%2)) -eq 1 ]]
+then	
 		player="user"
 		user="x"
 		computer="o"
-	else
+else
 		player="computer"
 		computer="x"
 		user="o"
-	fi
+fi
 }
 
 function switchPlayer(){
@@ -52,143 +52,152 @@ function rowColumnDiagonalWin() {
          [[ ${board[$k]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+8]} ]] ||
          [[ ${board[$k+2]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+6]} ]]
       then  
-         echo "player $player Win"
+         echo "$player Win"
          exit
       fi
       j=$((j+1))
    done
 }
 
-function computerRowWin(){
+function computerWinCondition(){
 	local symbol=$1
-	for((i=0;i<9;i=i+3))
+	if [ $flag -eq 0 ]
+	then 
+		rowWinBlock $1
+	fi
+	if [ $flag -eq 0 ]
+	then
+		columnWinBlock $1
+	fi
+	if [ $flag -eq 0 ]
+	then
+		diagonalWinBlock $1
+	fi
+}
+
+computerCondition(){
+	displayBoard
+	flag=1
+	((count++))
+}
+
+function columnWinBlock(){
+	local symbol=$1
+	for((i=0;i<9;i=i+1))
 	do
-		if [[ ${board[$i]}==$1 && ${board[$i+1]}==$1 && ${board[$i+2]}==$((i+3)) ]] 
+		if [[ ${board[$i]} == $symbol && ${board[$i+3]} == $symbol && ${board[$i+6]} == $((i+7)) ]]
 		then
-			board[$i+2]=$computer
-			displayBoard
-			exit
-		elif [[ ${board[$i]} == $1 && ${board[$i+2]} == $1 && ${board[$i+1]}==$((i+2)) ]]
+			board[$i+6]=$computer
+			computerCondition
+		elif [[ ${board[$i]} == $symbol && ${board[$i+6]} == $symbol && ${board[$i+3]} == $((i+4)) ]]
 		then
-			board[$i+1]=$computer
-			displayBoard
-			exit
-		elif [[ ${board[$i+1]} == $1 &&  ${board[$i+3]} == $1 && ${board[$i]} == $((i+1)) ]]
-	   	then
-	      	board[$i]=$computer
-	      	displayBoard
-		exit
-	   	fi 
+			board[$i+3]=$computer
+			computerCondition
+		elif [[ ${board[$i+3]} == $symbol && ${board[$i+6]} == $symbol && ${board[$i]} == $((i+1)) ]]
+		then
+			board[$i]=$computer
+			computerCondition
+		fi
 	done
 }
 
-function computerColumnWin(){
+function rowWinBlock(){
 	local symbol=$1
-	for((i=0;i<9;i=i+1))
-  	do
-        	if [[ ${board[$i]}==$1 && ${board[$i+3]}==$1 && ${board[$i+6]}==$((i+7)) ]] 
-        	then
-         		board[$i+6]=$computer
-         		displayBoard
-         		exit
-       		elif [[ ${board[$i]} == $1 && ${board[$i+6]} == $1 && ${board[$i+3]}==$((i+4)) ]]
-       		then
-         		board[$i+3]=$computer
-         		displayBoard
-         		exit
-       		elif [[ ${board[$i+6]} == $1 && ${board[$i+3]} == $1 && ${board[$i]} == $((i+1)) ]]
-       		then
-        		board[$i]=$computer
-         		displayBoard
-        	 	exit
-      		fi 
-     done
+	for((i=0;i<9;i=i+3))
+	do
+   		if [[ ${board[$i]} == $symbol && ${board[$i+1]} == $symbol && ${board[$i+2]} == $((i+3)) ]]
+   		then
+      			board[$i+2]=$computer
+     			computerCondition
+   		elif [[ ${board[$i]} == $symbol && ${board[$i+2]} == $symbol && ${board[$i+1]} == $((i+2)) ]]
+   		then
+      			board[$i+1]=$computer
+      			computerCondition
+   		elif [[ ${board[$i+1]} == $symbol && ${board[$i+2]} == $symbol && ${board[$i]} == $((i+1)) ]]
+   		then
+      			board[$i]=$computer
+      			computerCondition
+   		fi
+	done
 }
 
-function computerDiagonalWin(){
+function diagonalWinBlock(){
 	local symbol=$1
 	i=0
-	if [[ ${board[$i]}==$1 && ${board[$i+4]}==$1 && ${board[$i+8]}==$((i+9)) ]]
-	then
-		board[$i+8]=$computer
-		displayBoard
-		exit
-	elif [[ ${board[$i+4]}==$1 && ${board[$i+8]}==$1 && ${board[$i]}==$((i+1)) ]]
-	then
-		board[$i]=$computer
-		displayBoard
-		exit
-	elif [[ ${board[$i]}==$1 && ${board[$i+8]}==$1 && ${board[$i+4]}==$((i+5)) ]]
-	then
-		board[$i+4]=$computer
-		displayBoard
-		exit
-	fi
-	if [[ ${board[$i+2]}==$1 && ${board[$i+4]}==$1 && ${board[$i+6]}==$((i+7)) ]]
-	then
-		board[$i+6]=$computer
-		displayBoard
-		exit
-	elif [[ ${board[$i+2]}==$1 && ${board[$i+6]}==$1 && ${board[$i+4]}==$((i+5)) ]]
-	then
-		board[$i+4]=$computer
-		displayBoard
-		exit
-	elif [[ ${board[$i+4]}==$1 && ${board[$i+6]}==$1 && ${board[$i+2]}==$((i+3)) ]]
-	then
-		board[$i+2]=$computer
-		displayBoard
-		exit
-	fi
+   	if [[ ${board[$i+2]} == $symbol && ${board[$i+4]} == $symbol && ${board[$i+6]} == $((i+7)) ]]
+  	then
+     		board[$i+6]=$computer
+		computerCondition
+  	elif [[ ${board[$i+2]} == $symbol && ${board[$i+6]} == $symbol && ${board[$i+4]} == $((i+5)) ]]
+   	then
+     	 	board[$i+4]=$computer
+      	 	computerCondition
+   	elif [[ ${board[$i+4]} == $symbol && ${board[$i+6]} == $symbol && ${board[$i+2]} == $((i+3)) ]]
+   	then
+     	 	board[$i+2]=$computer
+      		computerCondition
+   	elif [[ ${board[$i]} == $symbol && ${board[$i+4]} == $symbol && ${board[$i+8]} == $((i+9)) ]]
+   	then
+      		board[$i+8]=$computer
+      		computerCondition
+   	elif [[ ${board[$i]} == $symbol && ${board[$i+8]} == $symbol && ${board[$i+4]} == $((i+5)) ]]
+   	then
+      		board[$i+4]=$computer
+      		computerCondition
+   	elif [[ ${board[$i+4]} == $symbol && ${board[$i+8]} == $symbol && ${board[$i]} == $((i+1)) ]]
+   	then
+      		board[$i]=$computer
+      		computerCondition
+   	fi
 }
 
-
 function userPlay(){
-	if [[ $count -lt $MAXCELL ]]
+	read -p "Enter Number Between 1 to 9:" position
+	if [[ ${board[$position-1]} -eq $position ]]
 	then
-		read -p "Enter Number Between 1 to 9:" position
-		if [[ ${board[$position-1]} -eq $position ]]
-		then
-			board[$position-1]=$user	
-			((count++))
-			displayBoard
-			rowColumnDiagonalWin
-		else
-			echo "invalid cell !"
-			userPlay
-		fi
-		computerPlay
+		board[$position-1]=$user	
+		((count++))
+		displayBoard
+		rowColumnDiagonalWin
 	else
-		echo "Game tie"
-		exit
+		echo "Cell already occupied !"
+		userPlay
 	fi
+	computerPlay
 }
 
 function computerPlay(){
-	if [[ $count -lt $MAXCELL ]]
+	flag=0
+	computerWinCondition $computer
+	computerWinCondition $user
+	if [[ $flag -eq 0 ]]
 	then
-		computerRowWin $computer
-		computerColumnWin $computer
-		computerDiagonalWin $computer
 		randomVariable=$((RANDOM%9+1))
 		echo "Random Number From Computer :$randomVariable"
 		if [[ ${board[randomVariable-1]} -eq $randomVariable ]]
 		then
 			board[$randomVariable-1]=$computer
-   		((count++))
-   		displayBoard
-			rowColumnDiagonalWin
+			((count++))
+			displayBoard
 		else
-			echo "invalid Cell !"
-   		computerPlay
+			echo "cell already occupied !"
+			computerPlay
 		fi
-		userPlay
-	else
-		echo "Game Tie"
-		exit
 	fi
+	rowColumnDiagonalWin
+	userPlay
 }
 
-displayBoard
-assignSymbol
-switchPlayer
+function checkValidCell(){
+	displayBoard
+	assignSymbol
+	while  [[ $count -lt $MAXCELL ]]
+	do
+		switchPlayer
+	done
+	if [[ $count -eq $MAXCELL ]]
+	then
+		echo "Tie game"
+	fi
+}
+checkValidCell
