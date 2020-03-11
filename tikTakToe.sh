f7c82deca184710ce2e7ddce1f59d96be2d2b581
shindepriti <pritishinde34@gmail.com>
2020-03-11 10:56:48 +0530

    ADDED:Function To Check Winner By Row Column And Diagonal

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -3,6 +3,9 @@ echo "***Wel-Come To Tik-Tak-Toe game***"
 
 declare -a board
 board=(1 2 3 4 5 6 7 8 9)
+count=0
+
+MAX_CELL=9
 
 function displayBoard(){
 	echo "-----------"
@@ -34,15 +37,39 @@ function whoPlayFirst(){
 }
 whoPlayFirst
 
-function getValidCell() {
-	read -p "Enter Number Between 1 to 9 :"  position
-	for((cell=0;cell<9;cell++))
+function rowColumnDiagonalWin() {
+	k=0
+	j=0
+	for((i=0;i<9;i=i+3))
 	do
-		if [ ${board[$cell]} -eq $position ]
+		if [[ ${board[$i]} == ${board[$i+1]} && ${board[$i+1]} == ${board[$i+2]} ]] ||
+			[[ ${board[$j]} == ${board[$j+3]} && ${board[$j+3]} ==  ${board[$j+6]} ]] 
+			[[ ${board[$k]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+8]} ]] ||
+			[[ ${board[$k+2]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+6]} ]]
+		then	
+			echo "player $player Win"
+			exit
+		fi
+		j=$((j+1))
+	done
+}
+
+
+function getValidCell() {
+	while [ $count -lt $MAX_CELL ]
+	do
+		read -p "Enter Number Between 1 to 9 :"  position
+		if [ ${board[$position-1]} -eq $position ]
 		then
-			board[cell]=$player
+			board[$position-1]=$player
+			((count++))
 			displayBoard
+			rowColumnDiagonalWin
+		else
+			echo "Invalid Cell!!"
 		fi
 	done
 }
 getValidCell
+
+
