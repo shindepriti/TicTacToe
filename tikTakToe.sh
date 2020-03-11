cb6771ceed9d8119ff647a4bd829a9412f307ca5
shindepriti <pritishinde34@gmail.com>
2020-03-11 12:20:00 +0530

    ADDED:Function To Computer Getting Its Turn And Play Like User

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -1,75 +1,107 @@
 #!/bin/bash -x
-echo "***Wel-Come To Tik-Tak-Toe game***"
+echo "Wel-Come To Tic-Tac-Toe game"
 
 declare -a board
 board=(1 2 3 4 5 6 7 8 9)
+
+#variable
 count=0
 
-MAX_CELL=9
+#CONSTANT
+MAXCELL=9
 
 function displayBoard(){
-	echo "-----------"
-	for ((i=0;i<9;i=i+3))
-	do
-		echo " ${board[$i]} | ${board[$i+1]} | ${board[$i+2]}"
-		echo "-----------"
-	done
+   echo "-----------"
+   for ((i=0;i<9;i=i+3))
+   do
+      echo " ${board[$i]} | ${board[$i+1]} | ${board[$i+2]}"
+      echo "-----------"
+   done
 }
-displayBoard
 
 function assignSymbol(){
-	if [ $((RANDOM%2)) -eq 1 ]
-	then
-		player="x"
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then	
+		player="user"
+		user="x"
+		computer="o"
 	else
-		player="o"
+		player="computer"
+		computer="x"
+		user="o"
 	fi
 }
-assignSymbol
 
-function whoPlayFirst(){
-	if [ $((RANDOM%2)) -eq 1 ]
+function switchPlayer(){
+	if [[ $player == "user" ]]
 	then
-		echo "player $player Play first"
+		userPlay
 	else
-		echo "player $player play first" 	
+		computerPlay
 	fi
 }
-whoPlayFirst
 
+#function to check winning condition for Row Column and Diagonal
 function rowColumnDiagonalWin() {
-	k=0
-	j=0
-	for((i=0;i<9;i=i+3))
-	do
-		if [[ ${board[$i]} == ${board[$i+1]} && ${board[$i+1]} == ${board[$i+2]} ]] ||
-			[[ ${board[$j]} == ${board[$j+3]} && ${board[$j+3]} ==  ${board[$j+6]} ]] 
-			[[ ${board[$k]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+8]} ]] ||
-			[[ ${board[$k+2]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+6]} ]]
-		then	
-			echo "player $player Win"
-			exit
-		fi
-		j=$((j+1))
-	done
+   k=0
+   j=0
+   for((i=0;i<9;i=i+3))
+   do
+      if [[ ${board[$i]} == ${board[$i+1]} && ${board[$i+1]} == ${board[$i+2]} ]] ||
+         [[ ${board[$j]} == ${board[$j+3]} && ${board[$j+3]} ==  ${board[$j+6]} ]] || 
+         [[ ${board[$k]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+8]} ]] ||
+         [[ ${board[$k+2]} == ${board[$k+4]} && ${board[$k+4]} == ${board[$k+6]} ]]
+      then  
+         echo "player $player Win"
+         exit
+      fi
+      j=$((j+1))
+   done
 }
 
-
-function getValidCell() {
-	while [ $count -lt $MAX_CELL ]
-	do
-		read -p "Enter Number Between 1 to 9 :"  position
-		if [ ${board[$position-1]} -eq $position ]
+function userPlay(){
+	if [[ $count -ne $MAXCELL ]]
+	then
+		read -p "Enter Number Between 1 to 9:" position
+		if [[ ${board[$position-1]} -eq $position ]]
 		then
-			board[$position-1]=$player
+			board[$position-1]=$user	
 			((count++))
 			displayBoard
 			rowColumnDiagonalWin
 		else
-			echo "Invalid Cell!!"
+			echo "invalid cell !"
+			userPlay
 		fi
-	done
+		computerPlay
+	else
+		echo "Game tie"
+		exit
+	fi
 }
-getValidCell
 
+function computerPlay(){
+	if [[ $count -ne $MAXCELL ]]
+	then
+		randomVariable=$((RANDOM%9+1))
+		echo "Random Number From Computer :$randomVariable"
+		if [[ ${board[randomVariable-1]} -eq $randomVariable ]]
+		then
+			board[$randomVariable-1]=$computer
+   		((count++))
+   		displayBoard
+			rowColumnDiagonalWin
+		else
+			echo "invalid Cell !"
+   		computerPlay
+		fi
+		userPlay
+	else
+		echo "Game Tie"
+		exit
+	fi
+}
 
+displayBoard
+assignSymbol
+switchPlayer
