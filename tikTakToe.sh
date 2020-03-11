13cf96683e960a34d48ec399ed52e7360455de6f
shindepriti <pritishinde34@gmail.com>
2020-03-11 09:31:06 +0530

    ADDED:Function To Assign Letter X and O To Player

--- tikTakToe.sh
+++ tikTakToe.sh
@@ -6,10 +6,20 @@ board=(1 2 3 4 5 6 7 8 9)
 
 function displayBoard(){
 	echo "-----------"
-for ((i=0;i<9;i=i+3))
-do
-	echo " ${board[$i]} | ${board[$i+1]} | ${board[$i+2]}"
-	echo "-----------"
-done
+	for ((i=0;i<9;i=i+3))
+	do
+		echo " ${board[$i]} | ${board[$i+1]} | ${board[$i+2]}"
+		echo "-----------"
+	done
 }
 displayBoard
+
+function assignSymbol(){
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		player="x"
+	else
+		player="o"
+	fi
+}
+assignSymbol
